b5cd3e6709faed52e0117ed448b3b96f8a8eb30e
yudithleon <yudithkleon@gmail.com>
2025-11-06 15:00:19 -0500

    los 3 botones funcionales

--- scripts.js
+++ scripts.js
@@ -1,13 +1,22 @@
-const button_incremet = document.getElementById("increment");
-const button_decrement = document.getElementById("decrement");
-const button_reset = document.getElementById("reset");
-const view = document.getElementsByClassName("view-count")
-let count = 0;
+const button_incremet = document.getElementById('increment')
+const button_decrement = document.getElementById('decrement')
+const button_reset = document.getElementById('reset')
+const view = document.getElementById('view-count')
+let count = 0
 
+view.innerHTML = count
 button_incremet.addEventListener('click', function () {
-      count = count + 1
-  console.log(count)
-  
-});
+  count = count + 1
+  view.innerHTML = count
+})
 
 
+button_decrement.addEventListener('click', function () {
+  count = count - 1
+  view.innerHTML = count
+})
+
+button_reset.addEventListener('click', function () {
+  count = 0
+  view.innerHTML = count
+})
