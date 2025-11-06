fd5e5006961e186ae27603f4be44f2f53a09cf98
yudithleon <yudithkleon@gmail.com>
2025-11-06 15:03:58 -0500

    funcion para validar el color

--- scripts.js
+++ scripts.js
@@ -1,22 +1,31 @@
-const button_incremet = document.getElementById('increment')
-const button_decrement = document.getElementById('decrement')
-const button_reset = document.getElementById('reset')
-const view = document.getElementById('view-count')
-let count = 0
+const button_incremet = document.getElementById("increment");
+const button_decrement = document.getElementById("decrement");
+const button_reset = document.getElementById("reset");
+const view = document.getElementById("view-count");
+let count = 0;
 
-view.innerHTML = count
-button_incremet.addEventListener('click', function () {
-  count = count + 1
-  view.innerHTML = count
-})
+view.innerHTML = count;
+button_incremet.addEventListener("click", function () {
+  count = count + 1;
+  view.innerHTML = count;
+});
 
+button_decrement.addEventListener("click", function () {
+  count = count - 1;
+  view.innerHTML = count;
+  validar_color(count);
+});
 
-button_decrement.addEventListener('click', function () {
-  count = count - 1
-  view.innerHTML = count
-})
+button_reset.addEventListener("click", function () {
+  count = 0;
+  view.innerHTML = count;
+  validar_color(count);
+});
 
-button_reset.addEventListener('click', function () {
-  count = 0
-  view.innerHTML = count
-})
+const validar_color = (count) => {
+  if (count >= 10) {
+    console.log("cambiar de color");
+  } else {
+    console.log("mantengo el color");
+  }
+};
