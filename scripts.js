e6c787f535e4343308ccf39ce226e41ad54077b0
yudithleon <yudithkleon@gmail.com>
2025-11-06 15:17:31 -0500

    estilos de botones

--- scripts.js
+++ scripts.js
@@ -4,22 +4,24 @@ const button_reset = document.getElementById("reset");
 const view = document.getElementById("view-count");
 let count = 0;
 
-view.innerHTML = count;
+view.innerHTML = `<h2 id="h2-count">${count}</h2>`;
 button_incremet.addEventListener("click", function () {
   count = count + 1;
-  view.innerHTML = count;
+  view.innerHTML = `<h2 id="h2-count">${count}</h2>`;
 });
 
 button_decrement.addEventListener("click", function () {
   count = count - 1;
-  view.innerHTML = count;
+
   validar_color(count);
+  view.innerHTML = `<h2 id="h2-count">${count}</h2>`;
 });
 
 button_reset.addEventListener("click", function () {
   count = 0;
-  view.innerHTML = count;
+
   validar_color(count);
+  view.innerHTML = `<h2>${count}</h2>`;
 });
 
 const validar_color = (count) => {
