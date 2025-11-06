b6cf654cd04fd9e8da7fb83edcb87fa583ffb45d
yudithleon <yudithkleon@gmail.com>
2025-11-06 14:56:22 -0500

    incrrement por consola

--- scripts.js
+++ scripts.js
@@ -1,4 +1,13 @@
+const button_incremet = document.getElementById("increment");
+const button_decrement = document.getElementById("decrement");
+const button_reset = document.getElementById("reset");
+const view = document.getElementsByClassName("view-count")
+let count = 0;
 
-const button_incremet = document.getElementById("incremet")
-const button_decrement = document.getElementById("increment")
-const button_reset = document.getElementById("reset")
+button_incremet.addEventListener('click', function () {
+      count = count + 1
+  console.log(count)
+  
+});
+
+
